9b86ed8e4a1ef0a5d341b976278bc32801a39472
DimP88 <dp88@ukr.net>
2025-04-18 22:36:22 +0300

    Оптимизаци header downх

--- src/Components/Layout/Header/HeaderDown/HeaderDown.jsx
+++ src/Components/Layout/Header/HeaderDown/HeaderDown.jsx
@@ -1,30 +1,38 @@
 import { Link } from "react-router-dom";
 import Icon from "../../../Icon/Icon";
+import { MdOutlineCrisisAlert } from "react-icons/md";
 
 const HeaderDown = () => {
   return (
     <div>
       <div className="header__down">
         <Link className="header__down-link" to="/">
-          <Icon id="kitchen-icon" /> Кухни
+          <Icon id="kitchen-icon" />
+          <span>Кухни</span>
         </Link>
         <Link className="header__down-link" to="/">
-          <Icon id="bedroom-icon" /> Спальни
+          <Icon id="bedroom-icon" />
+          <span>Спальни</span>
         </Link>
         <Link className="header__down-link" to="/">
-          <Icon id="livingroom-icon" /> Гостинные
+          <Icon id="livingroom-icon" />
+          <span>Гостинные</span>
         </Link>
         <Link className="header__down-link" to="/">
-          <Icon id="closet-icon" /> Прихожие
+          <Icon id="closet-icon" />
+          <span>Прихожие</span>
         </Link>
         <Link className="header__down-link" to="/">
-          <Icon id="office-icon" /> Офисная мебель
+          <Icon id="office-icon" />
+          <span>Офисная мебель</span>
         </Link>
         <Link className="header__down-link" to="/">
-          <Icon id="childrensroom-icon" /> Детская
+          <Icon id="childrensroom-icon" />
+          <span>Детская</span>
         </Link>
         <Link className="header__down-link" to="/" style={{ color: "red" }}>
-          Акция
+          <MdOutlineCrisisAlert />
+          <span>Акция</span>
         </Link>
       </div>
     </div>
